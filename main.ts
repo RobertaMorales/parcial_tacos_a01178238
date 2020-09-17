f633f193a801ca519b7234267fdf9c86dd13f138
RobertaMorales <70347219+RobertaMorales@users.noreply.github.com>
2020-09-17 15:36:49 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,15 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("Mujer")
     basic.showNumber(cant_playeras_hombre)
     basic.showString("Hombre")
-    basic.showString("Total")
-    basic.showNumber(cant_playeras_mujer + cant_playeras_hombre)
+    basic.showString("Total=")
+    basic.showNumber(cant_playeras_mujer * precio_playeras_mujer + cant_playeras_hombre * precio_playeras_hombre)
 })
+let precio_playeras_hombre = 0
+let precio_playeras_mujer = 0
 let cant_playeras_hombre = 0
 let cant_playeras_mujer = 0
 basic.showString("Roberta Morales A01178238")
 cant_playeras_mujer = randint(20, 200)
 cant_playeras_hombre = randint(20, 200)
-let precio_playeras_mujer = 250
-let precio_playeras_hombre = 200
+precio_playeras_mujer = 250
+precio_playeras_hombre = 200
